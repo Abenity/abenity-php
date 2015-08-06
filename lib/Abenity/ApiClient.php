0ff2eb40d0adce65c36b43338a30680af2bfe562
Mark Roland <mark@abenity.com>
2015-08-06 10:45:05 -0700

    Added support for SSO

--- lib/Abenity/ApiClient.php
+++ lib/Abenity/ApiClient.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * ApiClient
- *
- * PHP version 5
- *
- * @package  Abenity
- * @license  http://creativecommons.org/licenses/MIT/ MIT
- */
+
+namespace Abenity;
 
 /**
  * Abenity API interface
@@ -14,15 +8,10 @@
  * @package  Abenity
  * @license  http://creativecommons.org/licenses/MIT/ MIT
  */
-
-namespace Abenity;
-
 class ApiClient
 {
 
-    const API_VERSION = 'v1';
-
-    const DEFAULT_TIMEOUT = 2;
+    private $version;
 
     public $debug;
 
@@ -36,10 +25,20 @@ class ApiClient
 
     private $api_url = 'https://api.abenity.com';
 
+    private $public_key;
+
+    private $triple_des_key;
+
     /**
      * ApiClient constructor
+     * @param String $api_username Your API Username
+     * @param String $api_password Your API Password
+     * @param String $api_key Your API Key
+     * @param Integer $version The API version
+     * @param Integer $timeout Set how long to wait for the API to respond
+     * @return null
      */
-    public function __construct($api_username, $api_password, $api_key, $version = self::DEFAULT_TIMEOUT, $timeout = self::DEFAULT_TIMEOUT)
+    public function __construct($api_username, $api_password, $api_key, $version = 1, $timeout = 10)
     {
 
         // Set API Credentials
@@ -47,8 +46,33 @@ class ApiClient
         $this->api_password = $api_password;
         $this->api_key = $api_key;
 
+        $this->public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC8NVUZUtr2IHiFoY8s/qFGmZOIewAvg' .
+            'S4FMXWZ81Qc8lkAlZr9e171xn4PgKr+S7YsfCt+1XKyo5XmrJyaNUe/aRptB93NFn6RoFzExgfpkooxcHpWcP' .
+            'y+Hb5e0rwPDBA6zfyrYRj8uK/1HleFEr4v8u/HbnJmiFoNJ2hfZXn6Qw== phpseclib-generated-key';
+
+        // Set API Version
+        $this->version = 1;
+        if (is_numeric($version) && $version > 0 && $version < 2) {
+            $this->version = $version;
+        }
+
         // Set API Timeout
-        $this->timeout = $timeout;
+        $this->timeout = 10;
+        if (is_numeric($version)) {
+            $this->timeout = $timeout;
+        }
+
+        // Create new Crypt_RSA object
+        $this->rsa = new \Crypt_RSA;
+
+        // Define a code alphabet for generating strings
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        // Create Triple DES Key
+        for ($i = 0; $i < 24; $i++) {
+            $this->triple_des_key .= $codeAlphabet[$this->cryptoRandSecure(0, strlen($codeAlphabet))];
+        }
+
     }
 
     /**
@@ -57,13 +81,14 @@ class ApiClient
      * @param string $api_method The API method to be called
      * @param string $http_method The HTTP method to be used (GET, POST, PUT, DELETE, etc.)
      * @param array $data Any data to be sent to the API
-     * @return string An XML-formatted response
-     **/
+     * @return string A data-object of the response
+     */
     private function sendRequest($api_method, $http_method = 'GET', $data = null)
     {
 
         // Set the request type and construct the POST request
-        $data = array_merge( (array) $data,
+        $data = array_merge(
+            (array) $data,
             array(
                 'api_username' => $this->api_username,
                 'api_password' => $this->api_password,
@@ -81,7 +106,7 @@ class ApiClient
         $ch = curl_init();
 
         // Set the request
-        curl_setopt($ch, CURLOPT_URL, $this->api_url.$api_method);
+        curl_setopt($ch, CURLOPT_URL, $this->api_url . '/v' . $this->version . '/client' . $api_method);
 
         // Do not ouput the HTTP header
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -94,6 +119,9 @@ class ApiClient
 
         // This may be necessary, depending on your server's configuration
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        // Set the maximum number of seconds to allow cURL functions to execute
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
 
         // Send data
         if (!empty($postdata)) {
@@ -116,22 +144,28 @@ class ApiClient
         // Close cURL handle
         curl_close($ch);
 
-        // Parse XML response
-        $parsed_response = $this->parseResponse($curl_response, 'xml');
+        // Parse JSON response
+        $parsed_response = $this->parseResponse($curl_response, 'json');
 
         // Return parsed response
         return $parsed_response;
     }
 
     /**
-     *
-     **/
-    private function parseResponse($response, $format = 'xml')
+     * Parse the API response
+     * @param string $response The raw API response
+     * @param string $format The format to parse. ['json'|'xml']
+     */
+    private function parseResponse($response, $format = 'json')
     {
 
         $result = null;
 
-        if($format == 'xml'){
+        if ($format == 'json') {
+            $result = json_decode($response);
+        }
+
+        elseif ($format == 'xml') {
             $result = simplexml_load_string($response);
         }
 
@@ -139,15 +173,152 @@ class ApiClient
     }
 
     /**
+     * Create a random number. This is a more secure "rand" function
+     * Taken from: http://us1.php.net/manual/en/function.openssl-random-pseudo-bytes.php#104322
      *
+     * @param integer $min The random number lower bound
+     * @param integer $max The random number upper bound
+     * @return integer A random integer between the input bounds
      */
-    public function registerMember($member_profile)
+    private function cryptoRandSecure($min, $max)
     {
-        return $this->sendRequest('/client/register_member.xml', 'POST', $member_profile);
+
+        $range = $max - $min;
+
+        if ($range < 0) {
+            return $min;
+        }
+
+        $log = log($range, 2);
+
+        // length in bytes
+        $bytes = (int) ($log / 8) + 1;
+
+        // length in bites
+        $bits = (int) $log + 1;
+
+        // set all lower bits to 1
+        $filter = (int) (1 << $bits) - 1;
+
+        do {
+            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+            // discard irrelevant bits
+            $rnd = $rnd & $filter;
+        } while ($rnd >= $range);
+
+        return $min + $rnd;
     }
 
     /**
+     * Symmetrically encrypt a string of information
      *
+     * @param string $payload_string An input string
+     * @param string $iv An initialization vector for Triple-DES in CBC mode
+     * @return string A base64-encoded and url-encoded representation of the $payload_string
+     */
+    private function encryptPayload($payload_string, $iv)
+    {
+
+        $response = '';
+
+        $nameValuePairBinary = mcrypt_encrypt(MCRYPT_3DES, $this->triple_des_key, $payload_string, MCRYPT_MODE_CBC, $iv);
+        $nameValuePairText = base64_encode($nameValuePairBinary);
+        $response =  urlencode($nameValuePairText) . "decode";
+
+        return $response;
+    }
+
+    /**
+     * Asymmetrically encrypt a symmetrical encryption key
+     *
+     * @param string $triple_des_key A Triple DES (3DES) encryption key
+     * @return string A base64-encoded and url-encoded representation of the $triple_des_key
+     */
+    private function encryptCipher($triple_des_key)
+    {
+
+        $response = '';
+
+        $this->rsa->loadKey($this->public_key);
+        $encryptedSymmetricKeyBinary = $this->rsa->encrypt($triple_des_key);
+        $encryptedSymmetricKeyText = base64_encode($encryptedSymmetricKeyBinary);
+        $response = urlencode($encryptedSymmetricKeyText) . "decode";
+
+        return $response;
+    }
+
+    /**
+     * Sign a message using a private RSA key
+     *
+     * @param string $payload_string The message to be signed
+     * @param string $private_key An RSA private key
+     * @return string A base64-encoded and url-encoded hash of the $payload_string
+     */
+    private function signMessage($payload_string, $private_key)
+    {
+
+        $response = '';
+
+        $nameValuePairText = urldecode(substr($payload_string, 0, -6));
+        $messageDigest = md5($nameValuePairText);
+        $this->rsa->loadKey($private_key);
+        $signedMessageBinary = $this->rsa->encrypt($messageDigest);
+        $signedMessageText = base64_encode($signedMessageBinary);
+        $response = urlencode($signedMessageText) . "decode";
+
+        return $response;
+    }
+
+    /**
+     * Single Sign-On a member
+     *
+     * @return string The raw API response
+     * @param array $member_profile An array of key/value pairs that describes the member
+     * @param array $private_key Your RSA private key, used to sign your message
+     * @return string The raw API response
+     */
+    public function ssoMember($member_profile, $private_key)
+    {
+
+        // Convert member profile array to a HTTP query string
+        $payload_string = http_build_query($member_profile);
+
+        // Create Initialization Vector for symmetric encryption
+        $iv_size = mcrypt_get_iv_size(MCRYPT_3DES, MCRYPT_MODE_CBC);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        $Payload = $this->encryptPayload($payload_string, $iv);
+        $Cipher = $this->encryptCipher($this->triple_des_key);
+        $Signature = $this->signMessage($Payload, $private_key);
+
+        $data = array(
+            'Payload' => $Payload,
+            'Cipher' => $Cipher,
+            'Signature' => $Signature,
+            'Iv' => urlencode(base64_encode($iv)) . "decode"
+        );
+
+        return $this->sendRequest('/sso_member.json', 'POST', $data);
+    }
+
+    /**
+     * Register a Member
+     *
+     * @param array $member_profile An array of key/value pairs that describes the member
+     * @return string The raw API response
+     */
+    public function registerMember($member_profile)
+    {
+        return $this->sendRequest('/register_member.json', 'POST', $member_profile);
+    }
+
+    /**
+     * Authenticate a Username/Password as an active member. If valid,
+     * the response will contain a link to log the member into the Abenity program
+     *
+     * @param string $username The user's unique Abenity Username
+     * @param string $password The user's Abenity password
+     * @return string The raw API response
      */
     public function authenticateMember($username, $password)
     {
@@ -155,6 +326,6 @@ class ApiClient
             'username' => $username,
             'password' => $password
         );
-        return $this->sendRequest('/client/register_member.xml', 'POST', $data);
+        return $this->sendRequest('/encrypt_login.json', 'POST', $data);
     }
 }
